c1f1804570a504ee9e5c68344243762b8ff0df5d
saman-djafari <sami.djafari@gmail.com>
2024-02-28 10:43:30 -0500

    initializing react app

---
+++ src/App.js
@@ -0,0 +1,27 @@
+import logo from './logo.svg';
+import './App.css';
+
+function App() {
+  return (
+    <div className="App">
+      <header className="App-header">
+        <img src={logo} className="App-logo" alt="logo" />
+        <p>
+          AppSec Aquisition: Deploying React Workshop.
+        </p>
+        <a
+          className="App-link"
+          href="https://reactjs.org"
+          target="_blank"
+          rel="noopener noreferrer"
+        >
+          Link to the materials
+        </a>
+      </header>
+    <div>
+      <i>Presented by: Akash and Saman</i> 
+    </div></div>
+  );
+}
+
+export default App;
